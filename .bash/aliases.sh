08a17358fd8a95c6f88435af25aa540150cadd49
Ilya Zhigalko <i.zhigalko@gmail.com>
2016-04-05 10:30:43 +0300

    Add sudo alias for preserve user environment

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 if [[ `which grc 2> /dev/null` ]] ; then
-
     alias grc='grc --colour=auto'
     alias ping='grc ping'
     alias last='grc last'
@@ -13,9 +12,7 @@ if [[ `which grc 2> /dev/null` ]] ; then
     alias cat="grc cat"
     alias tail="grc tail"
     alias head="grc head"
-
 fi
-
 alias ls='ls --color=auto --all --human-readable --group-directories-first'
 alias grep='grep --color=auto'
 alias cp='cp --interactive --verbose --recursive --preserve=all'
@@ -24,5 +21,5 @@ alias rm='rm --recursive --interactive --verbose'
 alias du='du --human-readable'
 alias df='df --human-readable'
 alias free='free --human --total'
-
 [[ `which colordiff 2> /dev/null` ]] && alias diff='colordiff'
+alias sudo='sudo -E'
