d815ecc3f2ccbb8420047ecd95a37fb33398041d
Ilya Zhigalko <IOZhigalko@domclick.ru>
2017-10-30 11:25:45 +0300

    Fix commant prompt

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
+
+# Colors
+
+RED="\e[0;31m"
+RED_BOLD="\e[1;31m"
+GREEN="\e[1;32m"
+RESET="\e[0m"
+
 function get_git_branch() {
     local prefix="${RED_BOLD}git${RESET}::"
     local branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
-    [[ -n ${branch} ]] && echo -e "${prefix}${GREEN}${branch/#HEAD/detached}${RESET} "
+    [[ -n ${branch} ]] && printf "${prefix}${GREEN}${branch/#HEAD/detached}${RESET} "
 }
 
 function get_svn_branch() {
     local prefix="${RED_BOLD}svn${RESET}::"
-    local svn_url=$(svn info 2> /dev/null | grep -oP "(?<=Relative URL: \^)(.*)")
-    local branch=$(echo ${svn_url} | grep -oP "(?<=\/branches\/)([^\/]+)|(?<=\/tags\/)([^\/]+)|(?<=\/)(trunk)")
+    local svn_url=$(svn info 2> /dev/null | grep "^Relative URL: \^" | sed 's#^Relative URL: \^##g')
+    local branch=$(printf "${svn_url}" | perl -ne 'm/((?<=\/branches\/)([^\/]+)|(?<=\/tags\/)([^\/]+)|(?<=\/)(trunk))/; print "$1\n"')
 
     if [[ -n ${branch}  ]] ; then
 
-        echo -e "${prefix}${GREEN}${branch}${RESET} "
+        printf "${prefix}${GREEN}${branch}${RESET} "
 
     elif [[ -n ${svn_url} ]] ; then
 
-        echo -e "${prefix}${GREEN}${svn_url}${RESET} "
+        printf "${prefix}${GREEN}${svn_url}${RESET} "
 
     fi
 }
@@ -24,18 +32,9 @@ function get_svn_branch() {
 function get_virtual_env() {
     if [[ -n ${VIRTUAL_ENV} ]] ; then
         local venv=$(basename ${VIRTUAL_ENV})
-        echo -e "${RED_BOLD}venv${RESET}::${GREEN}${venv}${RESET} "
+        printf "${RED_BOLD}venv${RESET}::${GREEN}${venv}${RESET} "
     fi
 }
-
-# Colors
-
-RED="\e[0;31m"
-RED_BOLD="\e[1;31m"
-
-GREEN="\e[1;32m"
-
-RESET="\e[0m"
 
 # Prompt
 
